99df8c660db6cc7087732db30929ffce61cc8d09
Everett Witt <everwitt7@gmail.com>
2023-06-14 22:24:37 -0500

    functions `or`, `not`, `int_to_bits` and `AND` logic

--- 2015/p07.py
+++ 2015/p07.py
@@ -24,20 +24,38 @@ def rshift(bits, num):
 def bit_and(bits1, bits2):
     if len(bits1) != len(bits2):
         print(f"Bit Array Length Mismath, {len(bits1)=}, {len(bits2)=}")
-    and_arr = [0 for _ in range(len(bits1))]
+    arr = [0 for _ in range(len(bits1))]
 
     for idx in range(len(bits1)):
-        and_arr[idx] = bits1[idx] & bits2[idx]
+        arr[idx] = bits1[idx] & bits2[idx]
 
-    return and_arr
+    return arr
 
+def bit_or(bits1, bits2):
+    if len(bits1) != len(bits2):
+        print(f"Bit Array Length Mismath, {len(bits1)=}, {len(bits2)=}")
+    arr = [0 for _ in range(len(bits1))]
 
+    for idx in range(len(bits1)):
+        arr[idx] = bits1[idx] | bits2[idx]
+
+    return arr
+
+def bit_not(bits):
+    # XOR so 1 XOR 1 = 0 and 0 ^ 1 = 1... we can use xor to not a bit
+    arr = [bits[i] ^ 1 for i in range(len(bits))]
+    return arr
+
+def int_to_bits(num: int):
+    if num > 65535:
+        print(f"{num=} larger than unsigned 16 bit int")
+    return [(num >> i) & 1 for i in range(BIT_LENGTH-1, -1, -1)]
 
 
 def parser(file: str):
     contents = []
     with open(file, "r") as f:
-        for line in file:
+        for line in f:
             contents.append(line.rstrip())
     return contents 
 
@@ -54,10 +72,23 @@ def one(contents: list):
     for line in contents: 
         input, output = line.split(" -> ")
 
+        # its possible to have a literal instead of a signal as an argument
+        # 1 AND cc -> cd
         if AND in input:
+            # use string.isnumeric() to check if literal or signal
             sig1, sig2 = input.split(AND)
-            # signals[output] = signals[sig1].value & signals[sig2].value
-            signals[output] = signals[sig1] & signals[sig2]
+
+            if sig1.isnumeric() and sig2.isnumeric():
+                signals[output] = bit_and(int_to_bits(sig1), int_to_bits(sig2))
+            elif sig1 in signals and sig2.isnumeric():
+                signals[output] = bit_and(signals[sig1], int_to_bits(sig2))
+            elif sig1.isnumeric() and sig2 in signals:
+                signals[output] = bit_and(int_to_bits(sig1), signals[sig2])
+            elif sig1 in signals and sig2 in signals:
+                signals[output] = bit_and(signals[sig1], signals[sig2])
+            else:
+                print("gate should provide no signal until all inputs have a signal")
+
         elif OR in input:
             sig1, sig2 = input.split(OR)
             signals[output] = signals[sig1] | signals[sig2]
@@ -73,8 +104,6 @@ def one(contents: list):
             signals[output] = ~signals[sig] & 0xFFFF
         else:
             # set the value of the signal
-            # bit_rep = ctypes.c_uint16(int(input))
-            # signals[output] = bit_rep
             signals[output] = int(input)
 
     return signals
@@ -111,6 +140,22 @@ def test_and():
 
     assert t_ans == [0, 0, 0, 1]
 
+def test_or():
+    t_one = [0, 0, 1, 1]
+    t_two = [0, 1, 0, 1]
+    t_ans = bit_or(t_one, t_two)
+
+    assert t_ans == [0, 1, 1, 1]
+
+def test_not():
+    test = [0, 1, 0, 1]
+    ans = bit_not(test)
+    assert ans == [1, 0, 1, 0]
+
+def test_int_to_bits():
+    test = 49372
+    ans = int_to_bits(test)
+    assert ans == [1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0]
 
 if __name__ == "__main__":
     file = "./inputs/p07.txt"
@@ -161,5 +206,7 @@ if __name__ == "__main__":
     test_lshift()
     test_rshift()
     test_and()
+    test_or()
+    test_not()
 
 
